0f1665c43a8aa4193d50d1aca52bbb87115aeb0e
Morozhkaa <89974020+Morozhkaa@users.noreply.github.com>
2022-08-31 20:39:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pytesseract
 from PIL import Image
 
-img = Image.open("tele.png")
+img = Image.open("phone_number.png")
 
 # you need to download tesseract: https://github.com/UB-Mannheim/tesseract/wiki
 # then change the local path to tesseract.exe
