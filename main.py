646fd8bdda4f1e8aa4e0e86c206b2f6567ede109
Morozhkaa <89974020+Morozhkaa@users.noreply.github.com>
2022-08-31 20:31:09 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import pytesseract
+from PIL import Image
+
+img = Image.open("tele.png")
+
+# you need to download tesseract: https://github.com/UB-Mannheim/tesseract/wiki
+# then change the local path to tesseract.exe
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+text = pytesseract.image_to_string(img)
+
+with open("phone_number.txt", "r+") as text_file:
+    text_file.write(text)
