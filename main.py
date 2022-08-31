3116e64e8e34f6f7c40ce77fec0d00830f01ca46
Morozhkaa <89974020+Morozhkaa@users.noreply.github.com>
2022-08-31 21:39:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import pytesseract
 from PIL import Image
 
-img = Image.open("phone_number.png")
+# specify the title + extension of the picture you are translating into text
+
+#picture_name = 'phone_number.png'
+picture_name = 'eng_text.jpg'
+img = Image.open(picture_name)
+
 
 # you need to download tesseract: https://github.com/UB-Mannheim/tesseract/wiki
 # then change the local path to tesseract.exe
-
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+file_name = img.filename.split('.')[0]
+
 text = pytesseract.image_to_string(img)
 
-with open("phone_number.txt", "r+") as text_file:
+with open(f'{file_name}.txt', 'w') as text_file:
     text_file.write(text)
